b694b89a939cf036e2e55de3a8b9e5fef3f80c5d
Kemal Dag <gkemaldag@gmail.com>
2024-07-03 19:49:29 +0300

    Enable vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
 
+import { Analytics } from '@vercel/analytics/react'
+
 import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
@@ -22,6 +24,7 @@ export default function Nextra({ Component, pageProps }) {
         />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
